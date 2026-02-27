c96f7ee94662d1827adb7766426ec634685bc899
alexb <acbaldwin72@gmail.com>
2026-02-27 13:25:08 -0800

    reverse a string and fizzbuzz

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btnReverseString = new Button();
+            lblOutputLabel = new Label();
+            txtStringInput = new TextBox();
+            btnFizzBuzz = new Button();
+            SuspendLayout();
+            // 
+            // btnReverseString
+            // 
+            btnReverseString.Location = new Point(68, 95);
+            btnReverseString.Name = "btnReverseString";
+            btnReverseString.Size = new Size(163, 34);
+            btnReverseString.TabIndex = 0;
+            btnReverseString.Text = "Reverse String";
+            btnReverseString.UseVisualStyleBackColor = true;
+            btnReverseString.Click += reverseString;
+            // 
+            // lblOutputLabel
+            // 
+            lblOutputLabel.AutoSize = true;
+            lblOutputLabel.BackColor = SystemColors.ActiveBorder;
+            lblOutputLabel.BorderStyle = BorderStyle.FixedSingle;
+            lblOutputLabel.Location = new Point(452, 102);
+            lblOutputLabel.MinimumSize = new Size(150, 0);
+            lblOutputLabel.Name = "lblOutputLabel";
+            lblOutputLabel.Size = new Size(150, 27);
+            lblOutputLabel.TabIndex = 1;
+            // 
+            // txtStringInput
+            // 
+            txtStringInput.Location = new Point(258, 98);
+            txtStringInput.Name = "txtStringInput";
+            txtStringInput.Size = new Size(150, 31);
+            txtStringInput.TabIndex = 2;
+            // 
+            // btnFizzBuzz
+            // 
+            btnFizzBuzz.Location = new Point(68, 159);
+            btnFizzBuzz.Name = "btnFizzBuzz";
+            btnFizzBuzz.Size = new Size(163, 34);
+            btnFizzBuzz.TabIndex = 3;
+            btnFizzBuzz.Text = "Fizz Buzz";
+            btnFizzBuzz.UseVisualStyleBackColor = true;
+            btnFizzBuzz.Click += btnFizzBuzz_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(btnFizzBuzz);
+            Controls.Add(txtStringInput);
+            Controls.Add(lblOutputLabel);
+            Controls.Add(btnReverseString);
+            Name = "Form1";
+            Text = "Coding Test Practice";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label lblOutputLabel;
+        private TextBox txtStringInput;
+        private Button btnReverseString;
+        private Button btnFizzBuzz;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -6,5 +6,44 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void reverseString(object sender, EventArgs e)
+        {
+            string sInputText = txtStringInput.Text;
+            sInputText = ReverseStringDirect(sInputText);
+            lblOutputLabel.Text = sInputText;
+        }
+
+        public static string ReverseStringDirect(string s)
+        {
+            char[] array = new char[s.Length];
+            int forward = 0;
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                array[forward++] = s[i];
+            }
+            return new string(array);
+        }
+
+        private void btnFizzBuzz_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                } else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                } else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                } else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+        }
     }
 }
